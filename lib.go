4e0c567aa6896b4cbdcd5e60af841ad6584ba3e8
Brian Vess <brian.vess@gmail.com>
2025-10-31 19:30:28 -0400

    Add doc comments

--- lib.go
+++ lib.go
@@ -1,5 +1,8 @@
+// Package mathing provides math functions
 package mathing
 
+// Add returns the sum of two integers
+// https://www.mathisfun.com/numbers/addition/html
 func Add(a, b int) int {
 	return a + b
 }
