90ce3a8eae5cbebb4aa3c5f458cd54544c0b4279
Brian Vess <brian.vess@gmail.com>
2025-10-31 19:37:36 -0400

    fix incorrect url

--- lib.go
+++ lib.go
@@ -2,7 +2,7 @@
 package mathing
 
 // Add returns the sum of two integers
-// https://www.mathsisfun.com/numbers/addition/html
+// https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
