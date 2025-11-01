ee15971854bce11acc76456146b8c9ca7d545bf3
Brian Vess <brian.vess@gmail.com>
2025-10-31 20:28:13 -0400

    Update doc comments to reflect usage of generic number type

--- lib.go
+++ lib.go
@@ -7,7 +7,7 @@ type Number interface {
 	constraints.Integer | constraints.Float
 }
 
-// Add returns the sum of two integers
+// Add returns the sum of two numbers
 // https://www.mathsisfun.com/numbers/addition.html
 func Add[T Number](a, b T) T {
 	return a + b
