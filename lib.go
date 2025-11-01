805d585d9a8bd53f507ff72b0a0ce3d6426a93a2
Brian Vess <brian.vess@gmail.com>
2025-10-31 20:26:19 -0400

    Use generics

--- lib.go
+++ lib.go
@@ -1,8 +1,14 @@
 // Package mathing provides math functions
 package mathing
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add returns the sum of two integers
 // https://www.mathsisfun.com/numbers/addition.html
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
